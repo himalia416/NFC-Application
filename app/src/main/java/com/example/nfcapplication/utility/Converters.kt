27b85aa87547a6d71fbb4e0403b4d20aac9b9cdd
hiar <aryal.himali@gmail.com>
2023-03-28 09:27:24 +0200

    converter functions optimization

--- app/src/main/java/com/example/nfcapplication/utility/Converters.kt
+++ app/src/main/java/com/example/nfcapplication/utility/Converters.kt
@@ -1,23 +1,7 @@
 package com.example.nfcapplication.utility
 
-import java.math.BigInteger
-
-fun bytesToHex(bytes: ByteArray?): String {
-    val bigInteger = BigInteger(1, bytes)
-    val hexString = bigInteger.toString(16)
-    val paddingLength = ((bytes?.size ?: 0) * 2) - hexString.length
-    return if (paddingLength > 0) {
-        "0".repeat(paddingLength) + hexString
-    } else {
-        hexString
-    }
-}
+fun ByteArray.toHex(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
 
 fun serialNumberFormatter(input: String): String {
-    val sb = StringBuilder(input)
-
-    for (i in 2..sb.count()+3 step 3) {
-        sb.insert(i, ':')
-    }
-    return sb.toString().uppercase()
+    return input.chunked(2).joinToString(":").uppercase()
 }

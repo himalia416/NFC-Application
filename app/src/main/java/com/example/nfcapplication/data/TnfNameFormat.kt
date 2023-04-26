a903160096df9deba718a452e666578737147201
hiar <aryal.himali@gmail.com>
2023-04-26 16:02:26 +0200

    removed unused files

--- app/src/main/java/com/example/nfcapplication/data/TnfNameFormat.kt
+++ app/src/main/java/com/example/nfcapplication/data/TnfNameFormat.kt
@@ -1,14 +1,11 @@
 package com.example.nfcapplication.data
 
 enum class TnfNameFormatter(val tnf: String) {
-    /*
-    * For further details on this look into: https://developer.android.com/reference/android/nfc/NdefRecord#constants_1
-    * */
-    Empty("Empty"), // Indicates the record is empty.
+    Empty("Empty"),
     NFC_RTD("NFC Forum well-known type"),
-    RFC_2046("Media-type [RFC 2046]"), // media-type
+    RFC_2046("Media-type [RFC 2046]"),
     RFC_3986("Absolute URI [RFC 3986]"),
-    EXTERNAL_NFC_RTD("NFC Forum external type"), // Indicates the type field contains an external type name.
+    EXTERNAL_NFC_RTD("NFC Forum external type"),
     Unknown("Unknown"),
     Unchanged("Unchanged"),
     Reserved("Reserved");

b7f002e24ff2aa74bdb46499af3cec3d60fb9e0f
hiar <aryal.himali@gmail.com>
2023-06-05 15:43:37 +0200

    code refactor

--- serialization/src/main/java/no/nordicsemi/serialization/di/JsonAdapterModule.kt
+++ serialization/src/main/java/no/nordicsemi/serialization/di/JsonAdapterModule.kt
@@ -1,5 +1,13 @@
 package no.nordicsemi.serialization.di
 
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import no.nordicsemi.domain.nfcTag.MifareClassicTag
 import no.nordicsemi.domain.nfcTag.NdefTag
 import no.nordicsemi.domain.nfcTag.NfcTag
@@ -17,14 +25,6 @@ import no.nordicsemi.domain.nfcTag.ndef.record.URIRecord
 import no.nordicsemi.domain.nfcTag.ndef.record.Unknown
 import no.nordicsemi.serialization.domain.NfcJsonAdapter
 import no.nordicsemi.serialization.repository.NfcSerialization
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
@@ -37,21 +37,22 @@ object JsonAdapterModule {
         return Moshi.Builder()
             .add(
                 PolymorphicJsonAdapterFactory.of(NfcTag::class.java, "tag")
-                .withSubtype(NdefTag::class.java, "NdefTag")
-                .withSubtype(MifareClassicTag::class.java, "MifareClassicTag")
-                .withSubtype(OtherTag::class.java, "OtherTag")
+                    .withSubtype(NdefTag::class.java, "NdefTag")
+                    .withSubtype(MifareClassicTag::class.java, "MifareClassicTag")
+                    .withSubtype(OtherTag::class.java, "OtherTag")
             )
-            .add(PolymorphicJsonAdapterFactory.of(NdefRecordType::class.java, "recordType")
-                .withSubtype(TextRecord::class.java, "TextRecord")
-                .withSubtype(URIRecord::class.java, "URIRecord")
-                .withSubtype(SmartPoster::class.java, "SmartPoster")
-                .withSubtype(AlternativeCarrier::class.java, "AlternativeCarrier")
-                .withSubtype(HandoverCarrier::class.java, "HandoverCarrier")
-                .withSubtype(HandoverSelect::class.java, "HandoverSelect")
-                .withSubtype(HandoverReceive::class.java, "HandoverReceive")
-                .withSubtype(Unknown::class.java, "Unknown")
-                .withSubtype(AndroidPackage::class.java, "AndroidPackage")
-                .withSubtype(OtherExternalType::class.java, "OtherExternalType")
+            .add(
+                PolymorphicJsonAdapterFactory.of(NdefRecordType::class.java, "recordType")
+                    .withSubtype(TextRecord::class.java, "TextRecord")
+                    .withSubtype(URIRecord::class.java, "URIRecord")
+                    .withSubtype(SmartPoster::class.java, "SmartPoster")
+                    .withSubtype(AlternativeCarrier::class.java, "AlternativeCarrier")
+                    .withSubtype(HandoverCarrier::class.java, "HandoverCarrier")
+                    .withSubtype(HandoverSelect::class.java, "HandoverSelect")
+                    .withSubtype(HandoverReceive::class.java, "HandoverReceive")
+                    .withSubtype(Unknown::class.java, "Unknown")
+                    .withSubtype(AndroidPackage::class.java, "AndroidPackage")
+                    .withSubtype(OtherExternalType::class.java, "OtherExternalType")
             )
             .add(KotlinJsonAdapterFactory())
             .build()
@@ -59,7 +60,7 @@ object JsonAdapterModule {
 
     @Provides
     @Singleton
-    fun bindNfcJsonAdapter(jsonAdapter: JsonAdapter<NfcTag>) : NfcJsonAdapter{
+    fun bindNfcJsonAdapter(jsonAdapter: JsonAdapter<NfcTag>): NfcJsonAdapter {
         return NfcSerialization(jsonAdapter)
     }
 

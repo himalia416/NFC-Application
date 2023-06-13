e93b47b2e9a92bebb54917952d1da921bb7b4483
hiar <aryal.himali@gmail.com>
2023-06-13 10:56:59 +0200

    smart poster

--- profile_nfc/src/main/java/no/nordicsemi/profile_nfc/views/tagViews/ndefTag/record/DisplaySmartPosterRecord.kt
+++ profile_nfc/src/main/java/no/nordicsemi/profile_nfc/views/tagViews/ndefTag/record/DisplaySmartPosterRecord.kt
@@ -1,18 +1,23 @@
 package no.nordicsemi.profile_nfc.views.tagViews.ndefTag.record
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PictureInPicture
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.common.theme.NordicTheme
 import no.nordicsemi.domain.nfcTag.ndef.record.SmartPoster
 import no.nordicsemi.domain.nfcTag.ndef.record.TextRecord
 import no.nordicsemi.domain.nfcTag.ndef.record.URIRecord
-import no.nordicsemi.profile_nfc.R
+import no.nordicsemi.profile_nfc.component.RecordTitle
 import no.nordicsemi.profile_nfc.component.RowInCardView
 
 @Composable
@@ -20,29 +25,36 @@ fun DisplaySmartPosterRecord(
     smartPosterRecord: SmartPoster,
     index: Int
 ) {
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
+
     Column(modifier = Modifier.padding(8.dp)) {
-        Text(
-            text = stringResource(
-                id = R.string.record_name,
-                index + 1,
-                smartPosterRecord.recordName
-            ),
-            modifier = Modifier.padding(8.dp)
+        RecordTitle(
+            recordTitle = smartPosterRecord.recordName,
+            index = index,
+            recordIcon = Icons.Default.PictureInPicture,
+            isExpanded = isExpanded,
+            onExpandClicked = { isExpanded = !isExpanded }
         )
-        Column(modifier = Modifier.padding(8.dp)) {
-            smartPosterRecord.textRecord?.let {
-                DisplayTextRecord(textRecord = it, index = index)
-            }
-            DisplayUriRecord(uriRecord = smartPosterRecord.uriRecord, index = index)
-            smartPosterRecord.actionRecord?.let {
-                RowInCardView(
-                    title = "Action type",
-                    description = it.actionType
-                )
-                RowInCardView(
-                    title = "Action Data",
-                    description = it.actionData
-                )
+
+        if (isExpanded) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                smartPosterRecord.textRecord?.let {
+                    DisplayTextRecord(textRecord = it, index = index)
+                }
+                DisplayUriRecord(uriRecord = smartPosterRecord.uriRecord, index = index)
+                smartPosterRecord.actionRecord?.let {
+                    RowInCardView(
+                        title = "Action type",
+                        description = it.actionType
+                    )
+                    RowInCardView(
+                        title = "Action Data",
+                        description = it.actionData
+                    )
+                }
             }
         }
     }

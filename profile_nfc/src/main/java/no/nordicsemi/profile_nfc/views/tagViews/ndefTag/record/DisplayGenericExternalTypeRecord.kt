4fd58a043c36391fd1dfe587c878f14123ed4a24
hiar <aryal.himali@gmail.com>
2023-06-13 12:06:37 +0200

    GenericExternalType

--- profile_nfc/src/main/java/no/nordicsemi/profile_nfc/views/tagViews/ndefTag/record/DisplayGenericExternalTypeRecord.kt
+++ profile_nfc/src/main/java/no/nordicsemi/profile_nfc/views/tagViews/ndefTag/record/DisplayGenericExternalTypeRecord.kt
@@ -3,12 +3,19 @@ package no.nordicsemi.profile_nfc.views.tagViews.ndefTag.record
 import android.content.Context
 import android.content.pm.PackageManager
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Explicit
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -18,6 +25,7 @@ import no.nordicsemi.android.common.core.AppLauncher
 import no.nordicsemi.android.common.theme.NordicTheme
 import no.nordicsemi.domain.nfcTag.ndef.record.GenericExternalType
 import no.nordicsemi.profile_nfc.R
+import no.nordicsemi.profile_nfc.component.RecordTitle
 import no.nordicsemi.profile_nfc.component.RowInCardView
 
 @Composable
@@ -27,50 +35,55 @@ fun DisplayGenericExternalTypeRecord(
 ) {
     val context: Context = LocalContext.current
     val packageManager: PackageManager = context.packageManager
+    var isExpanded by rememberSaveable { mutableStateOf(false) }
 
     Column(modifier = Modifier.padding(8.dp)) {
-        Text(
-            text = stringResource(
-                id = R.string.record_name,
-                index + 1,
-                externalTypeRecord.recordName
-            ),
-            modifier = Modifier.padding(8.dp)
+        RecordTitle(
+            recordTitle = externalTypeRecord.recordName,
+            index = index,
+            recordIcon = Icons.Default.Explicit,
+            isExpanded = isExpanded,
+            onExpandClicked = { isExpanded = !isExpanded }
         )
-        Column(modifier = Modifier.padding(8.dp)) {
-            RowInCardView(
-                title = stringResource(id = R.string.record_type_name_format),
-                description = externalTypeRecord.typeNameFormat
-            )
-            RowInCardView(
-                title = stringResource(id = R.string.record_type),
-                description = externalTypeRecord.payloadType
-            )
-            RowInCardView(
-                title = stringResource(id = R.string.record_payload_len),
-                stringResource(
-                    id = R.string.bytes,
-                    externalTypeRecord.payloadLength.toString()
+        if (isExpanded) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                RowInCardView(
+                    title = stringResource(id = R.string.record_type_name_format),
+                    description = externalTypeRecord.typeNameFormat
                 )
-            )
-            Row {
-                Text(
-                    text = externalTypeRecord.domainType ?: stringResource(id = R.string.data),
-                    modifier = Modifier.padding(end = 16.dp),
-                    style = MaterialTheme.typography.titleMedium
+                RowInCardView(
+                    title = stringResource(id = R.string.record_type),
+                    description = externalTypeRecord.payloadType
                 )
-                Text(
-                    text = externalTypeRecord.payload,
-                    modifier = Modifier.clickable {
-                        AppLauncher.lunch(
-                            packageManager = packageManager,
-                            packageName = externalTypeRecord.payload,
-                            context = context
-                        )
-                    },
-                    color = MaterialTheme.colorScheme.secondaryContainer,
-                    style = MaterialTheme.typography.bodyLarge
+                RowInCardView(
+                    title = stringResource(id = R.string.record_payload_len),
+                    stringResource(
+                        id = R.string.bytes,
+                        externalTypeRecord.payloadLength.toString()
+                    )
                 )
+                Row {
+                    Text(
+                        text = externalTypeRecord.domainType ?: stringResource(id = R.string.data),
+                        modifier = Modifier.padding(end = 16.dp),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = externalTypeRecord.payload,
+                        modifier = Modifier.clickable {
+                            AppLauncher.lunch(
+                                packageManager = packageManager,
+                                packageName = externalTypeRecord.payload,
+                                context = context
+                            )
+                        },
+                        color = MaterialTheme.colorScheme.secondaryContainer,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
             }
         }
     }

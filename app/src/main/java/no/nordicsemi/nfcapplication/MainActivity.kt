8b9e3e282bc8edd2e25dde5f4c03e00135507f4c
hiar <aryal.himali@gmail.com>
2023-06-14 16:30:25 +0200

    selection across the app

--- app/src/main/java/no/nordicsemi/nfcapplication/MainActivity.kt
+++ app/src/main/java/no/nordicsemi/nfcapplication/MainActivity.kt
@@ -6,14 +6,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import no.nordicsemi.navigation.NfcDestinations
-import no.nordicsemi.profile_nfc.repository.NfcScanningManager
 import dagger.hilt.android.AndroidEntryPoint
 import no.nordicsemi.android.common.navigation.NavigationView
 import no.nordicsemi.android.common.theme.NordicActivity
 import no.nordicsemi.android.common.theme.NordicTheme
+import no.nordicsemi.navigation.NfcDestinations
+import no.nordicsemi.profile_nfc.repository.NfcScanningManager
 import java.util.*
 import javax.inject.Inject
 
@@ -29,7 +30,9 @@ class MainActivity : NordicActivity() {
 
         setContent {
             NordicTheme {
-                NavigationView(destinations = NfcDestinations)
+                SelectionContainer {
+                    NavigationView(destinations = NfcDestinations)
+                }
             }
         }
     }

d46c20500557c9c50229fe01da357c134246315d
hiar <aryal.himali@gmail.com>
2023-06-19 15:23:49 +0200

    new module in navigation

--- navigation/src/main/java/no/nordicsemi/navigation/NfcNavitagation.kt
+++ navigation/src/main/java/no/nordicsemi/navigation/NfcNavitagation.kt
@@ -2,9 +2,13 @@ package no.nordicsemi.navigation
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import no.nordicsemi.profile_nfc.ScanningDestination
-import no.nordicsemi.setting.NfcSettingsDestination
+import no.nordic.ui.NfcUiScreenDestination
+import no.nordicsemi.nfcscanner.ScanningDestination
+import no.nordicsemi.settings.NfcSettingsDestination
 import no.nordicsemi.welcome.NfcWelcomeDestination
 
 @RequiresApi(Build.VERSION_CODES.S)
-val NfcDestinations = ScanningDestination + NfcSettingsDestination + NfcWelcomeDestination
+val NfcDestinations = ScanningDestination +
+        NfcUiScreenDestination +
+        NfcSettingsDestination +
+        NfcWelcomeDestination

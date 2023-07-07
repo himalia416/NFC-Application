25e43823262be62954b56ad3b2e1a6a69f327668
himalia416 <himali.aryal@nordicsemi.no>
2023-07-07 16:20:47 -0500

    updated AppearanceParser

--- utils/src/main/java/no/nordisemi/utils/Ext.kt
+++ utils/src/main/java/no/nordisemi/utils/Ext.kt
@@ -13,21 +13,36 @@ fun String.toSerialNumber(): String = this.chunked(2).joinToString(":").uppercas
 /**
  * Converts ByteArray into the Hex format. Example: "54b683417f535dccaa0e454a09c248ca" is converted into "54 B6 83 41 7F 53 5D CC AA 0E 45 4A 09 C2 48 CA".
  */
-fun ByteArray.toPayloadData() : String = this.toHex().chunked(2).joinToString(" ").uppercase()
+fun ByteArray.toPayloadData(): String = this.toHex().chunked(2).joinToString(" ").uppercase()
 
 /**
  * Converts Hex String into atqa format. Example: 0044 is converted into 0x00444.
  */
-fun ByteArray.toAtqaFormat(): String = "0x${this.toHex() }"
+fun ByteArray.toAtqaFormat(): String = "0x${this.toHex()}"
 
 /**
  * Converts Hex String into SAK format. Example: 0 is converted into 0x00.
  */
-fun Short.toSakFormat() : String = "0x%02X".format(this)
+fun Short.toSakFormat(): String = "0x%02X".format(this)
 
 /**
  * Converts String into ByteArray.
  */
 fun String.toByteArray(): ByteArray = this.chunked(2)
-.map { it.toInt(16).toByte() }
-.toByteArray()
+    .map { it.toInt(16).toByte() }
+    .toByteArray()
+
+/**
+ * Converts ByteArray to Int.
+ */
+fun byteArrayToInt(bytes: ByteArray): Int {
+    if (bytes.size > 4) {
+        throw IllegalArgumentException("Byte array size must be 4 or less.")
+    }
+
+    var result = 0
+    for (element in bytes) {
+        result = (result shl 8) or (element.toInt() and 0xFF)
+    }
+    return result
+}
